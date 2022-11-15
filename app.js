5e1ef73a7f9c3d1e31379a82111d6c3839a8d034
Braeden <braeden.norman6@gmail.com>
2022-11-14 17:34:54 -0800

    ENH: implemented all users api calls from previous api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 const dotenv = require('dotenv');
 dotenv.config({
@@ -18,18 +19,19 @@ var cors = require("cors");
 var app = express();
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 app.use('/api/map', mapRouter);
 
 // catch 404 and forward to error handler
