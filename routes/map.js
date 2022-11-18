c87e5dce9acbc5e685dd9c69295be2f32ccf31b2
Braeden <braeden.norman6@gmail.com>
2022-11-17 21:24:00 -0800

    ENH: added getCoutryBorder api call and fixed getLocation

--- routes/map.js
+++ routes/map.js
@@ -20,16 +20,30 @@ router.get('/getEarthquakeData', async (req, res, next) =>  {
 
   router.get('/getLocations', async (req, res, next) =>  {
     const querySpec = {
-        query: `SELECT l.name, l.type, l.coordinates
-                FROM locations l`
+        query: `SELECT c.name, c.type, c.coordinates
+                FROM countries c`
     }
 
-    const container = getContainer("mapContent", "locations");
+    const container = getContainer("mapContent", "countries");
     const { resources } = await container.items.query(querySpec).fetchAll();
 
     responseMessage = {reply: "data found", locations: resources}
   
     
+    res.json(responseMessage);
+  });
+
+
+  router.get('/getCountryBorder', async (req, res, next) =>  {
+    const querySpec = {
+        query: `SELECT  "Feature" as type,
+                        {"name": c.name} as properties ,
+                        { "type": c.geometry_type, "coordinates" : c.border} as geometry
+                FROM countries c`
+    }
+    const container = getContainer("mapContent", "countries");
+    const { resources } = await container.items.query(querySpec).fetchAll();
+    responseMessage = {reply: "data found", borders: resources}
     res.json(responseMessage);
   });
 
