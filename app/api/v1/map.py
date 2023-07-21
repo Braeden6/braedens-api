a0d721b479b50b70ad3a38bd5467727d58a46447
BraedenHCL <braeden.norman6@gmail.com>
2023-07-21 10:25:57 -0700

    started app country information to map

--- app/api/v1/map.py
+++ app/api/v1/map.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from google.cloud import bigquery
 
 
 router = APIRouter( tags=["Map"])
@@ -51,3 +52,80 @@ async def get_earthquakes():
         }
     ]
 }
+
+
+
+
+
+@router.get("/countries")
+async def get_countries():
+    client = bigquery.Client()
+
+    # Define the query.
+    query = """
+        SELECT Country
+        FROM `personal-website-610c4.locations.country_data`
+        LIMIT 1000
+    """
+
+    # Run the query.
+    query_job = client.query(query)  # Make an API request.
+
+    # Fetch the results.
+    countries = query_job.result()
+
+    countries = [country.Country for country in countries]
+    return countries
+
+
+@router.get("/country/{country}")
+async def get_country(country: str):
+    client = bigquery.Client()
+
+    # Define the query.
+    query = """
+        SELECT *
+        FROM `personal-website-610c4.locations.country_data`
+        WHERE Country = @country
+        LIMIT 1000
+    """
+
+    # Set up the query parameters.
+    query_params = [
+        bigquery.ScalarQueryParameter("country", "STRING", country)
+    ]
+    
+    # Set up the job configuration.
+    job_config = bigquery.QueryJobConfig()
+    job_config.query_parameters = query_params
+
+    # Run the query.
+    query_job = client.query(query, job_config=job_config)  # Make an API request.
+
+    # Fetch the results.
+    result = query_job.result()
+
+    # Convert the result to a list of dictionaries to make it JSON serializable.
+    rows = [dict(row) for row in result]
+    
+    return rows
+
+
+@router.get("/countries/fix")
+async def fix_countries():
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    # Define the query.
+    query = """
+        UPDATE `personal-website-610c4.locations.country_data`
+        SET Country = TRIM(Country)
+    """
+
+    # Run the query.
+    query_job = client.query(query)  # Make an API request.
+
+    # Wait for the job to finish.
+    query_job.result()
+
+    print("Updated country names successfully.")
